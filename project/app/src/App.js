b78f4f27891533d2db942bd060cc5d10c447d5ec
Jaggu-123 <iit2017503@iiita.ac.in>
2019-10-12 05:51:35 +0530

    redux store attach

--- project/app/src/App.js
+++ project/app/src/App.js
@@ -1,8 +1,19 @@
 import React , {Component} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render(){
-    return <div>App</div>
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            Outpassed
+          </div>
+        </BrowserRouter>
+      </Provider>
+    )
   }
 }
 
